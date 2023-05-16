640e3998fae22b80375b1b47171b6bfd01a2aace
Huz Maryna <maryna.huz.dev@gmail.com>
2023-05-16 14:42:19 +0300

    - return type for the method reverseOneWord was changed; - a method reverse was extracted from the method reverseOneWord to avoid code duplication; - a method reverseString was created.

--- src/main/java/com/javajungle/Main.java
+++ src/main/java/com/javajungle/Main.java
@@ -1,21 +1,55 @@
 package com.javajungle;
 
-public class Main {
+import java.util.regex.Pattern;
 
-    private static char[] reverseOneWord(String word) {
+public class Main {
+    private static String reverseOneWord(String word) {
         char[] reversedWord = word.toCharArray();
-        int firstLetter = 0;
+        int firstLetter;
         int lastLetter = reversedWord.length - 1;
 
-        while(firstLetter<lastLetter){
-            char temp = reversedWord[firstLetter];
-            reversedWord[firstLetter] = reversedWord[lastLetter];
-            reversedWord[lastLetter] = temp;
+        for (firstLetter = 0; firstLetter < lastLetter; firstLetter++) {
 
-            firstLetter++;
+            if (isLetterChar(reversedWord[firstLetter]) && isLetterChar(reversedWord[lastLetter])) {
+                reverse(reversedWord, firstLetter, lastLetter);
+
+            } else if (!isLetterChar(reversedWord[firstLetter]) && isLetterChar(reversedWord[lastLetter])) {
+                firstLetter++;
+                reverse(reversedWord, firstLetter, lastLetter);
+
+            } else if (isLetterChar(reversedWord[firstLetter]) && !isLetterChar(reversedWord[lastLetter])) {
+                lastLetter--;
+                reverse(reversedWord, firstLetter, lastLetter);
+
+            } else {
+                firstLetter++;
+                lastLetter--;
+                reverse(reversedWord, firstLetter, lastLetter);
+            }
             lastLetter--;
         }
-        return reversedWord;
+        return new String(reversedWord);
+    }
+
+    private static void reverse(char[] reversedWord, int firstLetter, int lastLetter) {
+        char temp = reversedWord[firstLetter];
+        reversedWord[firstLetter] = reversedWord[lastLetter];
+        reversedWord[lastLetter] = temp;
+    }
+
+    private static String reverseString(String text) {
+        String[] separatedWords = text.split(" ");
+        StringBuilder reversedText = new StringBuilder();
+
+        for (String word : separatedWords) {
+            String reversedWord = reverseOneWord(word);
+            reversedText.append(reversedWord).append(" ");
+        }
+        return reversedText.toString().trim();
+    }
+
+    private static boolean isLetterChar(char character) {
+        return Pattern.matches("[a-zA-Z]", String.valueOf(character));
     }
 
 
@@ -23,5 +57,8 @@ public class Main {
 
         String word = "abcd";
         System.out.println(reverseOneWord(word));
+
+        String text = "a1bcd 2tfg!h";
+        System.out.println(reverseString(text));
     }
 }
