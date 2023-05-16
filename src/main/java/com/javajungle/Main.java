0c1105c673a7d7548825af3f3c2d08e84d2f3d7e
Huz Maryna <maryna.huz.dev@gmail.com>
2023-05-16 11:38:34 +0300

    a method "reverse one word" was created

--- src/main/java/com/javajungle/Main.java
+++ src/main/java/com/javajungle/Main.java
@@ -2,7 +2,26 @@ package com.javajungle;
 
 public class Main {
 
+    private static char[] reverseOneWord(String word) {
+        char[] reversedWord = word.toCharArray();
+        int firstLetter = 0;
+        int lastLetter = reversedWord.length - 1;
+
+        while(firstLetter<lastLetter){
+            char temp = reversedWord[firstLetter];
+            reversedWord[firstLetter] = reversedWord[lastLetter];
+            reversedWord[lastLetter] = temp;
+
+            firstLetter++;
+            lastLetter--;
+        }
+        return reversedWord;
+    }
+
+
     public static void main(String[] args) {
 
+        String word = "abcd";
+        System.out.println(reverseOneWord(word));
     }
 }
